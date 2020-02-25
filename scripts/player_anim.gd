0bf006ddffee6ac23e4cb0d2e18841f8aa42b8bb
unknown <ricknogueira1231@gmail.com>
2020-02-24 23:35:54 -0300

    'player_anim'

---
+++ scripts/player_anim.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "run-right":
+		$KinematicBody2D/sprite.flip_h = true
+		$AnimationPlayer.play("run-left")
+	elif anim_name == "run-left":
+		$KinematicBody2D/sprite.flip_h = false
+		$AnimationPlayer.play("run-right")
